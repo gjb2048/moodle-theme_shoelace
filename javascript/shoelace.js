cf75cd2f902b9ee91c227c1bcf1c3506a3ed6268
Gareth J Barnard <gjbarnard@gmail.com>
2013-06-04 16:46:52 +0100

    Work in progress.

--- javascript/shoelace.js
+++ javascript/shoelace.js
@@ -15,21 +15,35 @@
 
 M.theme_shoelace = M.theme_shoelace || {};
 
-M.theme_shoelace.blocks;
-M.theme_shoelace.content;
+M.theme_shoelace = {
+    blocks: null,
+    content: null,
+    Y: null
+}
 
 M.theme_shoelace.init = function(Y) {
     "use strict";
+    this.Y = Y;
     M.theme_shoelace.blocks = Y.one('#shoelace-blocks');
     if (M.theme_shoelace.blocks) {
-        M.theme_shoelace.blocks.replaceClass('span3', 'span1');
-        M.theme_shoelace.blocks.addClass('shoelace-closed');
         M.theme_shoelace.content = Y.one('#region-main');
-        if (M.theme_shoelace.content) {
-            M.theme_shoelace.content.replaceClass('span9', 'span11');
-        }
+        M.theme_shoelace.compress_blocks();
         M.theme_shoelace.blocks.on('mouseenter', M.theme_shoelace.blocks_enter);
         M.theme_shoelace.blocks.on('mouseleave', M.theme_shoelace.blocks_leave);
+    }
+};
+
+M.theme_shoelace.compress_blocks = function() {
+    M.theme_shoelace.blocks.replaceClass('span3', 'span1');
+    M.theme_shoelace.blocks.addClass('shoelace-closed');
+    if (M.theme_shoelace.content) {
+        M.theme_shoelace.content.replaceClass('span9', 'span11');
+    }
+    var titles = M.theme_shoelace.blocks.all('.header h2');
+    if (titles) {
+        titles.each(function(node) {
+            node.get('parentNode').replaceChild(M.theme_shoelace.fixTitleOrientation(node, node.get('text')), node);
+        });
     }
 };
 
@@ -46,11 +60,89 @@ M.theme_shoelace.blocks_enter = function(e) {
 M.theme_shoelace.blocks_leave = function(e) {
     "use strict";
     e.preventDefault();
-    M.theme_shoelace.blocks.replaceClass('span3', 'span1');
-    M.theme_shoelace.blocks.addClass('shoelace-closed');
-    if (M.theme_shoelace.content) {
-        M.theme_shoelace.content.replaceClass('span9', 'span11');
-    }
+    M.theme_shoelace.compress_blocks();
 };
 
-YUI().use('node', 'event', M.theme_shoelace.init);
+// Modified from /blocks/dock.js
+M.theme_shoelace.fixTitleOrientation = function(title, text) {
+    var Y = this.Y;
+
+    var title = Y.one(title);
+
+    if (Y.UA.ie > 0 && Y.UA.ie < 8) {
+        // IE 6/7 can't rotate text so force ver
+        M.str.langconfig.thisdirectionvertical = 'ver';
+    }
+
+    var clockwise = false;
+    switch (M.str.langconfig.thisdirectionvertical) {
+        case 'ver':
+            // Stacked is easy
+            return title.setContent(text.split('').join('<br />'));
+        case 'ttb':
+            clockwise = true;
+            break;
+        case 'btt':
+            clockwise = false;
+            break;
+    }
+
+    if (Y.UA.ie == 8) {
+        // IE8 can flip the text via CSS but not handle transform. IE9+ can handle the CSS3 transform attribute.
+        title.setContent(text);
+        title.setAttribute('style', 'writing-mode: tb-rl; filter: flipV flipH;display:inline;');
+        title.addClass('filterrotate');
+        return title;
+    }
+
+    // We need to fix a font-size - sorry theme designers.
+    var fontsize = '11px';
+    var transform = (clockwise) ? 'rotate(45deg)' : 'rotate(-45deg)';
+    var test = Y.Node.create('<h2><span class="transform-test-node" style="font-size:'+fontsize+';">'+text+'</span></h2>');
+    title.insert(test, 0);
+    var width = test.one('span').get('offsetWidth') * 1.2;
+    var height = test.one('span').get('offsetHeight');
+    test.remove();
+
+    title.setContent(text);
+    title.addClass('css3transform');
+
+    // Move the title into position
+    title.setStyles({
+        'margin' : '0',
+        'padding' : '0',
+        'position' : 'relative',
+        'fontSize' : fontsize,
+        'width' : width,
+        'top' : width/2
+    });
+
+    // Positioning is different when in RTL mode.
+    if (right_to_left()) {
+        title.setStyle('left', width/2 - height);
+    } else {
+        title.setStyle('right', width/2 - height);
+    }
+
+    // Rotate the text
+    title.setStyles({
+        'transform' : transform,
+        '-ms-transform' : transform,
+        '-moz-transform' : transform,
+        '-webkit-transform' : transform,
+        '-o-transform' : transform
+    });
+
+    var container = Y.Node.create('<div></div>');
+    container.append(title);
+    container.setStyle('height', width + (width / 4));
+    container.setStyle('position', 'relative');
+    return container;
+};
+
+YUI().use('node', 'event', function(Y) {
+    console.log(M);
+    if (!M.cfg.isediting) {
+        M.theme_shoelace.init(Y);
+    }
+});
