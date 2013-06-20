581cb27e6a8f45fe1eaa3cf53cada8ed5fb09188
Gareth J Barnard <gjbarnard@gmail.com>
2013-06-20 19:08:24 +0100

    V2.5.2 - Beta

--- layout/columns2.php
+++ layout/columns2.php
@@ -100,13 +100,15 @@ echo $OUTPUT->doctype() ?>
     </header>
 
     <div id="page-content" class="row-fluid">
-        <section id="region-main" class="span9<?php if ($left) { echo ' pull-right'; } ?>">
-            <?php
-            echo $OUTPUT->course_content_header();
-            echo $OUTPUT->main_content();
-            echo $OUTPUT->course_content_footer();
-            ?>
-        </section>
+        <div id="region-main-shoelace" class="span9<?php if ($ltr) { echo ' pull-right'; } ?>">
+                <section id="region-main" class="row-fluid">
+                <?php
+                echo $OUTPUT->course_content_header();
+                echo $OUTPUT->main_content();
+                echo $OUTPUT->course_content_footer();
+                ?>
+            </section>
+        </div>
         <?php
         $classextra = '';
         if ($left) {
