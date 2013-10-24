e279321a05b00d0971caf35fd42e9e520afc99af
Gareth J Barnard <gjbarnard@gmail.com>
2013-10-24 23:29:23 +0100

    V2.5.3

--- less/make_shoe.bat
+++ less/make_shoe.bat
@@ -2,3 +2,4 @@ call copy variables-shoelace.less "../../bootstrapbase/less/bootstrap/variables.
 call cd ../../bootstrapbase/less
 call recess --compile --compress ../../shoelace/less/moodleallshoelace.less > ../../shoelace/style/moodle.css
 call recess --compile --compress ../../shoelace/less/editorallshoelace.less > ../../shoelace/style/editor.css
+call cd ../../shoelace/less
